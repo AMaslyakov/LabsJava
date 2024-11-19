2a31fe0d5e83480bdabfca226606372e926c48d3
AMaslyakov <a.v.masliakov@urfu.me>
2024-11-19 14:49:22 +0500

    Added solutions for 15, 16 self-study tasks

---
+++ example16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class example16 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Введите чиcло: ");
+    int num2 = input.nextInt();
+    int num1 = num2 - 1;
+    int num3 = num2 + 1;
+    int tmp = num1 + num2 +num3;
+    int num4 = tmp * tmp;
+
+    System.out.printf(
+        "%d\n%d\n%d\n%d\n",
+        num1, num2, num3, num4
+        );
+
+    input.close();
+  }
+}
