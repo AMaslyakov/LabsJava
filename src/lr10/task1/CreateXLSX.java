a304b93284501ab464d00a7e2678d8b3b902221f
AMaslyakov <a.v.masliakov@urfu.me>
2025-03-13 22:59:17 +0500

    Add Task1 with 6 examples for Lab10

---
+++ src/lr10/task1/CreateXLSX.java
@@ -0,0 +1,40 @@
+package src.lr10.task1;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateXLSX {
+    public static void main(String[] args) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        XSSFSheet sheet = workbook.createSheet("Товары");
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("Товар");
+        headerRow.createCell(1).setCellValue("Характеристики");
+        headerRow.createCell(2).setCellValue("Цена");
+
+        Row dataRow1 = sheet.createRow(1);
+        dataRow1.createCell(0).setCellValue("Книга");
+        dataRow1.createCell(1).setCellValue("Жанр: Фантастика, Автор: Френк Герберт");
+        dataRow1.createCell(2).setCellValue(1500.00);
+
+        Row dataRow2 = sheet.createRow(2);
+        dataRow2.createCell(0).setCellValue("Компьютер");
+        dataRow2.createCell(1).setCellValue("Процессор: Intel Core i9," +
+                " ОЗУ: 32 ГБ DDR4, Видеокарта: NVIDIA RTX 4080");
+        dataRow2.createCell(2).setCellValue(300000.00);
+
+        String filepath = "src/lr10/task1/goods.xlsx";
+        FileOutputStream outputStream = new FileOutputStream(filepath);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+
+        System.out.println("Данные записаны в файл:" + filepath);
+
+    }
+}
