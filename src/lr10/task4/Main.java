b27b6e4cf1acaf584173b5ae6bf48ee577de7c66
AMaslyakov <a.v.masliakov@urfu.me>
2025-03-17 21:22:15 +0500

    Completed Task4 Lab10

---
+++ src/lr10/task4/Main.java
@@ -0,0 +1,18 @@
+package src.lr10.task4;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            NewsParser paper = new NewsParser();
+            paper.print_news();
+            paper.save_news();
+        } catch (UnknownHostException e){
+            System.out.println("Не возможно открыть сайт!");
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
