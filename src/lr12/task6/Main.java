3650b1be36a08dc1dee2ade343f0853b8df99088
AMaslyakov <a.v.masliakov@urfu.me>
2025-04-09 21:11:50 +0500

    Completed task6 for Lab12

--- src/lr12/task6/Main.java
+++ src/lr12/task6/Main.java
@@ -11,22 +11,31 @@ public class Main {
     return result;  }
 
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Введите размер массива для поиска минимального числа.");
     long[] big_array = getArray(input.nextInt());
+    long sumValue;
+    if(big_array.length < 10){
+        System.out.println("Массив не достаточной длины, для запуска потоков");
+        sumValue = Arrays.stream(big_array).min().orElseThrow();
+        System.out.println("Сумма элементов массива: " +  sumValue);
+        return;
+    }
+    System.out.println("Сложение элементов массива с помощью потоков!");
+    try {
+          sumValue =calculateSum(big_array);
+          System.out.println("Cумма элементов массива: " +  sumValue);
+    } catch (Exception e){
+        e.printStackTrace();
 
-  int cpus_num = defCpusNum();
+  }}
 
-  int MIN_LENGTH_ON_CORE = 4*cpus_num;
+  public static long calculateSum(long[] big_array) throws InterruptedException {
+    int cpus_num = getCpusCount();
 
-  if(big_array.length < MIN_LENGTH_ON_CORE){
-     System.out.println("Сумма значений массива: ");
-     System.out.println(Arrays.stream(big_array).sum());
-  }else{
-    int offset = big_array.length % cpus_num == cpus_num - 1  ?
-                 (big_array.length / cpus_num)+1 :
-                 big_array.length/cpus_num;
+    int offset = big_array.length / cpus_num;
+    int reminder = big_array.length % cpus_num;
 
     List<Thread> threads = new ArrayList<>();
     List<Result> results = new ArrayList<>();
@@ -36,10 +45,12 @@ public class Main {
     }
 
     for(int i=0; i<cpus_num; i++){
+      int diff = reminder > 0 ? 1 : 0;
       long[] chunk = Arrays.copyOfRange(big_array,
               i*offset,
-              i < cpus_num - 1 ? (i+1)*offset : big_array.length);
+              (i+1)*offset + diff);
       threads.add(new Thread(new Summator(chunk, results.get(i))));
+      reminder--;
     }
 
     for(Thread thread: threads){
@@ -52,10 +63,10 @@ public class Main {
 
     long arraySum = results.stream().map(Result::toInt).reduce(Long::sum).orElseThrow();
     System.out.println("Сумма значений массива: " +  arraySum);
-    }
+    return arraySum;
   }
 
-    private static int defCpusNum(){
+    private static int getCpusCount(){
       return Runtime.getRuntime().availableProcessors();
     }
 }
