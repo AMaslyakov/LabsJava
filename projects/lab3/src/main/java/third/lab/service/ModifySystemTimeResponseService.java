c1c04501a4d013d15d655f98aac5c4cf5f7547d0
AMaslyakov <a.v.masliakov@urfu.me>
2025-08-31 09:39:07 +0500

    Lab3 Add Interface ModifyResponseService and two realisation

---
+++ projects/lab3/src/main/java/third/lab/service/ModifySystemTimeResponseService.java
@@ -0,0 +1,21 @@
+package third.lab.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import third.lab.model.Response;
+import util.DateTimeUtil;
+
+@Service
+@Qualifier("ModifySystemTimeResponseService")
+public class ModifySystemTimeResponseService implements ModifyResponseService{
+
+    @Override
+    public Response modify(Response response){
+        response.setSystemTime(DateTimeUtil.getCustomFormat().format(new Date()));
+        return response;
+    }
+
+}
