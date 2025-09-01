fa2b3e6e7c549ccd2cbdeb929461f8a851fb495a
AMaslyakov <a.v.masliakov@urfu.me>
2025-09-01 20:01:47 +0500

    Lab3, additional functionality, logs INFO in service

--- projects/lab3/src/main/java/third/lab/service/ModifyOperationUidResponseService.java
+++ projects/lab3/src/main/java/third/lab/service/ModifyOperationUidResponseService.java
@@ -5,8 +5,10 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import third.lab.model.Response;
 
+@Slf4j
 @Service
 @Qualifier("ModifyOperationUidResponseService")
 public class ModifyOperationUidResponseService implements ModifyResponseService{
@@ -14,6 +16,7 @@ public class ModifyOperationUidResponseService implements ModifyResponseService{
     @Override
     public Response modify(Response response){
         UUID uuid = UUID.randomUUID();
+        log.info("Изменяется поле operationUid объекта response");
         response.setOperationUid(uuid.toString());
         
         return response;
